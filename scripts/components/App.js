9027ed4a8e0becdcfa6c61607ef02c10cdbd3476
Chuck4Git <JoshGC3@gmail.com>
2016-11-21 22:25:34 -0500

    Experimented with Firebase. No view changes

--- scripts/components/App.js
+++ scripts/components/App.js
@@ -21,13 +21,30 @@ class App extends React.Component {
 		this.state = {}
 		}
 
+		componentWillMount() {
+			// this.firebaseRef = new Firebase('https://blinding-heat-3145.firebaseio.com/');
+			// this.firebaseRef.push({'a': 1, 'b': 2, 'c': 3})
+			// var that = this;
+			// this.firebaseRef.once('value', function(snapshot){
+			// 	var foos = [];
+			// 	snapshot.forEach(function(data) {
+			// 		var foo = {
+			// 			i: data.val()
+			// 		}
+			// 		foos.push(foo);
+			// 	});
+			// 	that.setState({foos: foos})
+			// });
 
+		}
 
 		render() {
 
 			return (
 				<div style={style}>
 					<p>This is from App.js</p>
+
+					
 					<Link to='main-view'>Create New Route</Link>
 					<Link to='saved-routes'>View Saved Route</Link>
 					{this.props.children}
